18d091e4443fb3ba0686bb1fcdb1b1ff68e596cf
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-08-27 14:38:42 +0100

    Create App component in seperate file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        )
-    }
-
-}
+import './App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'))
